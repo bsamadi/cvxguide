9b868be7e05bd28b12506b68b1aa6da76991111b
bsamadi <behzad@mechatronics3d.com>
2014-03-21 22:02:22 -0400

    Changing the theme to simple

--- makeslides.bat
+++ makeslides.bat
@@ -1,2 +1,2 @@
 @echo off
-pandoc --biblio src\main.bib --csl src\ieee.csl --include-in-header css\slides.css --mathjax -t revealjs -V theme=beige -s --slide-level 2 cvxguideslides.md -o cvxguideslides.html
+pandoc --biblio src\main.bib --csl src\ieee.csl --include-in-header css\slides.css --mathjax -t revealjs -V theme=simple -s --slide-level 2 cvxguideslides.md -o cvxguideslides.html
