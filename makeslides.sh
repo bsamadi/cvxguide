f5e814e0430144df47a63ebf8996352b3867f593
bsamadi <bsamadi@nubonetics.com>
2020-05-29 08:52:23 -0400

    Remove Reveal.js

--- makeslides.sh
+++ makeslides.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pandoc --biblio src/main.bib --csl src/ieee.csl --include-in-header css/slides.css --mathjax -t revealjs -V theme=simple -s --slide-level 2 cvxguideslides.md -o cvxguideslides.html
+pandoc --biblio src/main.bib --csl src/ieee.csl --include-in-header css/slides.css --mathjax -t revealjs -V theme=white -V revealjs-url=https://revealjs.com -s --slide-level 2 cvxguideslides.md -o cvxguideslides.html
