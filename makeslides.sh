86969f43218c85723f4e5097c3262f45516939a2
bsamadi <bsamadi@nubonetics.com>
2020-05-29 09:15:53 -0400

    Changing Reveal to 3.9.2

--- makeslides.sh
+++ makeslides.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pandoc --biblio src/main.bib --csl src/ieee.csl --include-in-header css/slides.css --mathjax -t revealjs -V theme=white -s --slide-level 2 cvxguideslides.md -o cvxguideslides.html
+pandoc --biblio src/main.bib --csl src/ieee.csl --include-in-header css/slides.css --mathjax -t revealjs -V theme=white -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ -s --slide-level 2 cvxguideslides.md -o cvxguideslides.html
